348e34e7f993af8a43ed6a945ee1ac7a581078af
YunkoRoman <romanyunko1996@gmail.com>
2021-03-08 02:52:09 +0200

    add migrations

---
+++ dataBase/migration.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    port: process.env.DB_PORT_DEV,
+    dialect: process.env.DB_DIALECT_DEV,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    dialect: process.env.DB_DIALECT,
+  },
+};
---
+++ dataBase/migrations/createDB.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+    up: (queryInterface, Sequelize) => queryInterface.createTable('topic', {
+        id: {
+            type: Sequelize.DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        text: Sequelize.DataTypes.TEXT,
+
+    }).then(() => queryInterface.createTable('message', {
+        id: {
+            type: Sequelize.DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        text: {
+            type: Sequelize.DataTypes.TEXT('long')
+        },
+        topic_id: {
+            type: Sequelize.DataTypes.INTEGER
+        },
+        user_name: {
+            type: Sequelize.DataTypes.STRING(128)
+        },
+        date: {
+            type: Sequelize.DataTypes.DATE
+        }
+    })),
+
+    down: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
+        queryInterface.dropTable('topic', {transaction: t}),
+        queryInterface.dropTable('message', {transaction: t}),
+
+    ])),
+};
